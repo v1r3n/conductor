bdc0322295dddfd1883f0f5ee203f60a741bb2cd
Viren Baraiya <virenx@gmail.com>
2023-09-22 18:11:07 -0700

    fix the formatting

--- java-sdk/src/test/java/com/netflix/conductor/sdk/workflow/def/WorkflowCreationTests.java
+++ java-sdk/src/test/java/com/netflix/conductor/sdk/workflow/def/WorkflowCreationTests.java
@@ -22,7 +22,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
